4fbb2c6b5b689b218c1cbb00baebbd1db29fbdc7
Dan Moore <dan@fusionauth.io>
2021-11-15 17:22:45 -0700

    Initial commit, pulling over from examples from AWS

---
+++ resources/widgets.js
@@ -0,0 +1,56 @@
+// Copyright 2010-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+//
+// This file is licensed under the Apache License, Version 2.0 (the "License").
+// You may not use this file except in compliance with the License. A copy of the
+// License is located at
+//
+// http://aws.amazon.com/apache2.0/
+//
+// This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+// OF ANY KIND, either express or implied. See the License for the specific
+// language governing permissions and limitations under the License.
+//snippet-start:[cdk.typescript.widgets]
+//snippet-start:[cdk.typescript.widgets.imports]
+const AWS = require('aws-sdk');
+
+const bucketName = process.env.BUCKET;
+
+//snippet-start:[cdk.typescript.widgets.exports_main]
+exports.main = async function(event, context) {
+  try {
+    var method = event.httpMethod;
+    // Get name, if present
+    var widgetName = event.path.startsWith('/') ? event.path.substring(1) : event.path;
+
+    if (method === "GET") {
+      // GET / to get the names of all widgets
+      if (event.path === "/") {
+        const data = "addddbc"
+        var body = {
+          widgets: data
+        };
+        return {
+          statusCode: 200,
+          headers: {},
+          body: JSON.stringify(body)
+        };
+      }
+    }
+
+    // We got something besides a GET
+    return {
+      statusCode: 400,
+      headers: {},
+      body: "We only accept GET, POST, and DELETE, not " + method
+    };
+  } catch(error) {
+    var body = error.stack || JSON.stringify(error, null, 2);
+    return {
+      statusCode: 400,
+      headers: {},
+      body: body
+    }
+  }
+}
+//snippet-end:[cdk.typescript.widgets.exports_main]
+//snippet-end:[cdk.typescript.widgets]
