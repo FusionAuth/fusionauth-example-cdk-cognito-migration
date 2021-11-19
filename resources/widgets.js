6ce8d8e9d0eb57bc1db6c27729f59d42b2f14c96
Dan Moore <dan@fusionauth.io>
2021-11-19 11:07:08 -0700

    Cleanup, added handling of users without proper setup.

--- resources/widgets.js
+++ resources/widgets.js
@@ -9,6 +9,7 @@
 // This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
 // OF ANY KIND, either express or implied. See the License for the specific
 // language governing permissions and limitations under the License.
+
 const AWS = require('aws-sdk');
 
 const clientId = process.env.CLIENT_ID;
@@ -23,7 +24,6 @@ function processOneAttribute(attributes, name) {
   }
   return null
 }
-
 
 function processUserJSON(json) {
   // map the json returned by cognito to FusionAuth compatible json
@@ -73,11 +73,8 @@ function processUserJSON(json) {
   userJSON.user.registrations[0] = { applicationId: "85a03867-dccf-4882-adde-1a79aeec50df" }
   userJSON.user.tenantId = fusionAuthTenantId 
 
-  console.log("abc")
-  console.log(json)
-  console.log(attributes)
+)
   const emailVerified = processOneAttribute(attributes, "email_verified")
-  console.log(emailVerified)
   if (emailVerified != null) {
     userJSON.user.verified = (emailVerified == "true")
   }
@@ -100,11 +97,6 @@ exports.main = async function(event, context) {
     const method = event.httpMethod;
     // Get name, if present
     if (method === "POST") {
-// TODO
-//rename stuff
-//set up cognito app with correct perms
-//- no client secret
-//- only password path
 
       if (event.path === "/") {
         const incomingBody = JSON.parse(event.body)
@@ -120,27 +112,24 @@ exports.main = async function(event, context) {
             }
          };
 
-// TODO what about users newly created
-// { "ChallengeName": "NEW_PASSWORD_REQUIRED", "Session": "AYABeByvCvgZ2jJj38MpkJhH9AcAHQABAAdTZXJ2aWNlABBDb2duaXRvVXNlclBvb2xzAAEAB2F3cy1rbXMAS2Fybjphd3M6a21zOnVzLWVhc3QtMjo0MTc1Njc5MDM0Njk6a2V5LzVjZDI0ZDRjLWVjNWItNGU4Ny05MGI2LTVkODdkOTZmY2RkMgC4AQIBAHiDgLf0JMCQi10Le18mRz-AZa5fOI0Qay69WItbdSFa4AFpV6oGQc9yVIQJW8pGlkWuAAAAfjB8BgkqhkiG9w0BBwagbzBtAgEAMGgGCSqGSIb3DQEHATAeBglghkgBZQMEAS4wEQQMhlROx4r4McMR27t-AgEQgDu652m-3rprXj3Tb3nhoy9pmXBtu-kVP4Mc_R6SA9wjcztzY6-OdKI5GALb1uOas1vWKLR8Hv56k8ASiQIAAAAADAAAEAAAAAAAAAAAAAAAAADPzDDn6ouMJOotjm-U6HnS_____wAAAAEAAAAAAAAAAAAAAAEAAAFJZRfaoUFIZNFB85OfqDVjReL1tTrTeMyj60l9sBzex5hvEBcxkmjCNX6dP8s6q4z0yQNe3ILWlIiu4-XUw0umMpZ7nMdHEffkXYwfZ5lG4halkH_I_VGAKSBzkCmk2fRLuVvO2klH7bED1V0qruZ-1t0GAkFFEHOhYrXaTteTfZYFr3eo2WVuVCkFm75q_xts2JwOxjcyr87shLDSyRZQnHRZIJrjanGssU7iMeCHRvGsp8J1lZdgJ-gb_oz629uU-DpmWNK5pooPw3wcNtFnuQvmOaxkQhkvrtfFZWLPNMiEfg8BF1Jd325J-BatLRqoSprL2KR0XY8MY0jj-vIcvrHgZbRAgnRgcYdEiFM6blfWrv7eXurFd0M1luhhcRcdcCQsGU5wWeJTbO1LLNnWihNh-la45TqSUnJ9uueN8LgUaKJQGwIF3hxThbhAPjriJEWPb_1wX1Y6", "ChallengeParameters": { "USER_ID_FOR_SRP": "testuser", "requiredAttributes": "[\"userAttributes.gender\",\"userAttributes.given_name\",\"userAttributes.nickname\",\"userAttributes.middle_name\",\"userAttributes.name\",\"userAttributes.zoneinfo\",\"userAttributes.profile\",\"userAttributes.family_name\",\"userAttributes.email\",\"userAttributes.phone_number\",\"userAttributes.website\",\"userAttributes.preferred_username\",\"userAttributes.locale\",\"userAttributes.picture\",\"userAttributes.address\"]", "userAttributes": "{\"zoneinfo\":\"\",\"website\":\"\",\"address\":\"\",\"gender\":\"\",\"profile\":\"\",\"preferred_username\":\"\",\"locale\":\"\",\"given_name\":\"\",\"middle_name\":\"\",\"picture\":\"\",\"updated_at\":\"1637107972\",\"name\":\"\",\"nickname\":\"\",\"phone_number\":\"\",\"family_name\":\"\",\"email\":\"\"}" } }
-
-// if challengename is not blank, then something is wrong and we cna't proceed
 
     	const res = await cognito.initiateAuth(params).promise()
         var jsonResponse = {}
         if (res.AuthenticationResult && res.AuthenticationResult.AccessToken) {
     	   const res2 = await cognito.getUser({AccessToken:res.AuthenticationResult.AccessToken}).promise();
            jsonResponse = processUserJSON(res2)
-        }
+        
         return {
             statusCode: 200,
             headers: {},
             body: JSON.stringify(jsonResponse)
         };
+        } else if (res.ChallengeName) {
+          console.log("Unable to log user in. Got response: "+res.ChallengeName+" and am unsure how to handle.")
+        }
      }
      }
      }
-
-// TODO check header for security value auth
 
     return {
       statusCode: 400,
@@ -158,4 +147,3 @@ exports.main = async function(event, context) {
     }
   }
 }
-
